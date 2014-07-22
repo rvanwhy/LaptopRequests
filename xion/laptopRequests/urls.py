60f5d533c23b022ddac578c289ef3ccba8e7880c
field <randyvanwhy@gmail.com>
2014-07-22 14:30:06 -0600

    Fixed up some CSS and modified the URL Schema

--- xion/laptopRequests/urls.py
+++ xion/laptopRequests/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from laptopRequests import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^archived$', views.archived, name='archived'),
+    url(r'^([0-9]*)$', views.getDetails, name='getDetails')
 ]
--- xion/laptopRequests/views.py
+++ xion/laptopRequests/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render_to_response
 from laptopRequests.models import request
 
 def index(req):
+  entries = request.objects.all().filter(status__in=['Ready','Testing', 'New'])
+  return render_to_response('index.html', {'requests': entries, 'archiveButton' : 'Show'})
+
+def archived(req):
   entries = request.objects.all()
-  color = {'New': 'gray', 'Started': 'blue', 'Testing': 'orange', 'Ready':'green', 'Archive': 'pink'}
-  return render_to_response('index.html', {'requests': entries, 'color': color, 'admin': 0})
+  return render_to_response('index.html', {'requests': entries, 'archiveButton':'Hide'})
+
+def getDetails(req, requestNumber):
+  entries = request.objects.all().filter(id=requestNumber)
+  return render_to_response('request.html', {'requests': entries})
