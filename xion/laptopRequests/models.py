82e9a511c6a2822ffafc1e5c1ccd16f642bad51f
field <randyvanwhy@gmail.com>
2014-07-22 15:53:16 -0600

    All links should be working now. Notes to come next

--- xion/laptopRequests/models.py
+++ xion/laptopRequests/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+#'ENUM' which outlines the possible statuses of equipment
 STATI = (
 	('New', 'New'),
 	('Started', 'Started'),
@@ -8,6 +9,7 @@ STATI = (
 	('Archive', 'Archive')
 )
 
+#Experiment types
 EXPR = (
   ('RT130', 'RT130'),
   ('Q330', 'Q330'),
@@ -15,12 +17,14 @@ EXPR = (
   ('Other', 'Other')
 )
 
+#Computer types
 COMP = (
   ('PC', 'PC'),
   ('MAC', 'Mac'),
   ('NOPREF', 'NOPREF'),
 )
 
+#Possible equipment owners
 OWN = (
   ('PASSCAL', 'PASSCAL'),
   ('FLEX ARRAY', 'FLEX ARRAY'),
@@ -28,6 +32,32 @@ OWN = (
 )
 
 class request(models.Model):
+    """Defines the request object for the database.
+    This outline is copied from the old laptop database which
+    this project succeeded.
+
+    status -- the status of the laptop defined by a list of choices
+    data -- the date the request was entered
+    name -- the name of the requestor
+    email -- the email of the requestor
+    password -- optional password
+    notify -- whether or not to notify by email
+    readyby -- the date that the laptop is due
+    nlaptops -- the number of laptops Required
+    experimentName -- the name of the experimentName
+    experimentNumber -- the experiment number
+    location -- where the experiment is to take replaces
+    backpack -- whether or not to include a backpack
+    typeX -- the experiment types
+    typeL -- the laptop type
+    owner -- the equipment owners
+    ninstr -- number of instruments on the experiment
+    diskspace -- the amount of diskspace to include
+    media -- lists any other media to include
+    software -- lists all needed software
+    notes -- any additional notes
+    """
+
     status = models.CharField(default='New', blank=True, max_length=40, choices=STATI)
     date = models.DateField()
     name = models.CharField(max_length=40)
@@ -50,4 +80,23 @@ class request(models.Model):
     notes = models.TextField(blank=True, default='')
 
     def __unicode__(self):
+      """Outlines how the object is named in Django Admin.
+      This replaces the old 'request object' title with
+      something more descriptive:
+
+        EXPERIMENT_NAME -- ENTRY_DATE (YYYY-MM-DD)
+      """
+
       return "{0} -- {1}".format(self.experimentName, self.date)
+
+
+class note(models.Model):
+  """ Outlines the laptop notes which are stored separately from requests
+  """
+
+  reqid = models.IntegerField()
+  item = models.CharField(max_length=40)
+  barcode = models.CharField(max_length=40)
+  specs = models.TextField(blank=True)
+  status = models.CharField(default='New', blank=True, max_length=40, choices=STATI)
+  comments = models.TextField(blank=True)
--- xion/laptopRequests/views.py
+++ xion/laptopRequests/views.py
@@ -1,15 +1,32 @@
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.core.exceptions import ObjectDoesNotExist
 
 from laptopRequests.models import request
 
+
 def index(req):
-  entries = request.objects.all().filter(status__in=['Ready','Testing', 'New'])
+  """Loads the main page which contains all
+  non-archived laptop requests.
+  """
+  entries = request.objects.filter(status__in=['Ready','Testing', 'New'])
   return render_to_response('index.html', {'requests': entries, 'archiveButton' : 'Show'})
 
 def archived(req):
+  """Loads the main page including archived
+  requests.
+  """
   entries = request.objects.all()
   return render_to_response('index.html', {'requests': entries, 'archiveButton':'Hide'})
 
 def getDetails(req, requestNumber):
-  entries = request.objects.all().filter(id=requestNumber)
-  return render_to_response('request.html', {'requests': entries})
+  """Attempts to grab a request's details and
+  display them for the user. If no requst is found,
+  it simply displays 'Request not found!'
+  """
+  try:
+    entry = request.objects.get(id=requestNumber)
+  except ObjectDoesNotExist:
+    return HttpResponse('<h1>Request not found!</h1><p>Are you sure your url is correct?</p>')
+
+  return render_to_response('request.html', {'request': entry})
